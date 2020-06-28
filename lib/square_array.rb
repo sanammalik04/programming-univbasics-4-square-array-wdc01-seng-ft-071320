ec2e0e9a03a6c652f9d1491a464c8eb07ac28ef1
Sanam Malik <sanam.malik04@gmail.com>
2020-06-28 21:55:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
-counter = 0 
-while counter < array.length do 
-  array[counter] = array[counter] **2
-  counter += 1
+  counter = 0 
+  while counter < array.length do 
+    array[counter] = array[counter] **2
+    counter += 1
+  end
 end
